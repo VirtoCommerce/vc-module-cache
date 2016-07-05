a80a00807c4e67ba82ea5f689f0f57d5713d7ae3
Tatarincev <et@virtoway.com>
2016-07-05 16:03:20 +0200

    Initial module

---
+++ VirtoCommerce.CacheModule.Web/Module.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Common.Logging;
+using Microsoft.Practices.Unity;
+using VirtoCommerce.Platform.Core.Modularity;
+using VirtoCommerce.Platform.Core.Settings;
+
+namespace VirtoCommerce.CacheModule.Web
+{
+    public class Module : ModuleBase
+    {     
+        private readonly IUnityContainer _container;
+
+        public Module(IUnityContainer container)
+        {
+            _container = container;
+        }
+
+        #region IModule Members
+
+        public override void Initialize()
+        {
+      
+        }
+
+        public override void PostInitialize()
+        {
+          
+        }
+        #endregion
+    }
+}
