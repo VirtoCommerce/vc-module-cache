5f496f7541468e520fd01a061bede082cc6704b3
Tatarincev <et@virtoway.com>
2017-04-21 11:59:52 +0200

    Add new api methods GET api/cache/stats - to retrieve cache usage statistics GET api/cache/reset - to reset cache

---
+++ VirtoCommerce.CacheModule.Web/Controllers/Api/CacheModuleController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Web.Http;
+using System.Web.Http.Description;
+using CacheManager.Core;
+using VirtoCommerce.CacheModule.Web.Model;
+using VirtoCommerce.CacheModule.Web.Security;
+using VirtoCommerce.CacheModule.Web.Services;
+using VirtoCommerce.Platform.Core.Web.Security;
+
+namespace VirtoCommerce.CacheModule.Web.Controllers.Api
+{    
+    [RoutePrefix("api")]
+    public class CacheModuleController : ApiController
+    {
+        private readonly ICacheManager<object> _cacheManager;
+        private readonly IChangesTrackingService _changesTrackingService;
+
+        public CacheModuleController(IChangesTrackingService changesTrackingService, ICacheManager<object> cacheManager)
+        {
+            _changesTrackingService = changesTrackingService;
+            _cacheManager = cacheManager;
+        }
+
+        /// <summary>
+        /// Return current platform cache usage stats 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("cache/stats")]
+        [ResponseType(typeof(string))]
+        public IHttpActionResult GetCacheStats()
+        {
+            var retVal = new List<CacheHandleStat>();
+            foreach (var handle in _cacheManager.CacheHandles)
+            {
+                var stat = new CacheHandleStat
+                {
+                    Name = handle.Configuration.Name,
+                    ItemsCount = handle.Count,
+                    Hits = handle.Stats.GetStatistic(CacheManager.Core.Internal.CacheStatsCounterType.Hits),
+                    Misses = handle.Stats.GetStatistic(CacheManager.Core.Internal.CacheStatsCounterType.Misses),
+                };
+                retVal.Add(stat);
+            }
+            return Ok(retVal.ToArray());
+        }
+
+        /// <summary>
+        /// Reset current platform cache
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("cache/reset")]
+        [ResponseType(typeof(string))]
+        [CheckPermission(Permission = CachePredefinedPermissions.ResetCache)]
+        public IHttpActionResult CacheReset()
+        {
+            _cacheManager.Clear();       
+            return Ok();
+        }
+
+        /// <summary>
+        /// Force set changes last modified date 
+        /// </summary>
+        /// <param name="forceRequest">Force changes request</param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("changes/force")]
+        [ResponseType(typeof(void))]
+        [CheckPermission(Permission = CachePredefinedPermissions.ResetCache)]
+        public IHttpActionResult ForceChanges(ForceChangesRequest forceRequest)
+        {
+            _changesTrackingService.Update(forceRequest?.Scope, DateTime.UtcNow);
+            return Ok();
+        }
+
+        /// <summary>
+        /// Get last modified date for given scope
+        /// </summary>
+        /// <param name="scope"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("changes/lastmodifieddate")]
+        [ResponseType(typeof(LastModifiedResponse))]
+        public IHttpActionResult GetLastModifiedDate(string scope = null)
+        {
+            var result = new LastModifiedResponse
+            {
+                Scope = scope,
+                LastModifiedDate = _changesTrackingService.GetLastModifiedDate(scope),
+            };
+
+            return Ok(result);
+        }
+    }
+}
---
+++ VirtoCommerce.CacheModule.Web/Model/CacheHandleStat.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VirtoCommerce.CacheModule.Web.Model
+{
+    public class CacheHandleStat
+    {
+        /// <summary>
+        /// Handle name
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// The number of hits.
+        /// </summary>
+        public long Hits { get; set; }
+        /// <summary>
+        /// The number of misses.
+        /// </summary>
+        public long Misses { get; set; }
+        //Gets the number of items the cache  currently maintains.
+        public long ItemsCount { get; set; }
+    }
+}
