385092037c3d1072dd34574ab2c3aa0171199db6
Tatarincev <et@virtoway.com>
2017-05-15 15:25:46 +0200

    [publish] 1.0.13

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -14,8 +14,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("VirtoCommerce")]
 [assembly: AssemblyCopyright("Copyright © VirtoCommerce 2011-2017")]
 
-[assembly: AssemblyFileVersion("1.0.12.0")]
-[assembly: AssemblyVersion("1.0.12.0")]
+[assembly: AssemblyFileVersion("1.0.13.0")]
+[assembly: AssemblyVersion("1.0.13.0")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
